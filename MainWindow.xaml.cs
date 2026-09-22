490a08a2fdf73d1449bbcdda05d212b276fe1667
@sokolov <sokolov@safib.ru>
2026-09-22 08:30:52 +0300

    feat: иконка прилежения

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,7 +20,12 @@ public sealed partial class MainWindow : Window
         ExtendsContentIntoTitleBar = true;
         SetTitleBar(AppTitleBar);
         AppWindow.TitleBar.PreferredHeightOption = TitleBarHeightOption.Tall;
-        AppWindow.SetIcon("Assets/AppIcon.ico");
+
+        if (File.Exists(Path.Combine(AppContext.BaseDirectory, "Assets", "AppIcon.ico")))
+        {
+            AppWindow.SetIcon(Path.Combine(AppContext.BaseDirectory, "Assets", "AppIcon.ico"));
+        }
+
         AppWindow.Resize(new Windows.Graphics.SizeInt32(770, 780));
 
         if (AppWindow.Presenter is OverlappedPresenter presenter)
